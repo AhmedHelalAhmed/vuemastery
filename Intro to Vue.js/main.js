3740e90189bd6593da12c75277f3b8c9aa56c36b
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-02-22 22:32:36 +0200

    Attribute Binding

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -2,6 +2,10 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Sockets',
-        description: 'A pair of warm, fuzzy socks'
+        description: 'A pair of warm, fuzzy socks',
+        image: './assets/vmSocks-green.jpg',
+        altText: 'A pair of socks',
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+
     }
 });
