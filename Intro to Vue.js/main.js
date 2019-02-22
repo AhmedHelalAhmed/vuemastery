53ff11eb6e205bca3708cb1db2db1d4a9cffe7ba
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-02-22 22:47:52 +0200

    Conditional Rendering

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -6,6 +6,7 @@ var app = new Vue({
         image: './assets/vmSocks-green.jpg',
         altText: 'A pair of socks',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-
+        inventory: 0,
+        onSale: true
     }
 });
