fa287f13718e11de871577a68af1e604982f0f32
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-02-22 23:41:22 +0200

    Class & Style Binding

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -6,8 +6,8 @@ var app = new Vue({
         image: './assets/vmSocks-green.jpg',
         altText: 'A pair of socks',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inventory: 0,
-        inStock: true,
+        inventory: 100,
+        inStock: false,
         onSale: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
@@ -25,6 +25,7 @@ var app = new Vue({
         ],
         sizes: ["small", "medium", "large"],
         cart: 0,
+
 
 
     },
