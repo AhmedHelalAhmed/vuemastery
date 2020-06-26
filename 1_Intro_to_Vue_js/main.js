91e97de234d093da6c1a821adf84559953da945f
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-06-26 22:29:02 +0200

    finalize first 6 videoes

---
+++ 1_Intro_to_Vue_js/main.js
@@ -0,0 +1,48 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    brand: "Vue Mastery",
+    product: "Sockets",
+    description: "A pair of warm, fuzzy socks",
+    image: "./assets/vmSocks-green.jpg",
+    altText: "A pair of socks",
+    link:
+      "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+    inventory: 100,
+    inStock: true,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green.jpg",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue.jpg",
+      },
+    ],
+    sizes: ["small", "medium", "large"],
+    cart: 0,
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+  },
+});
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # vuemastery
+
+## tasks of 1_Intro_to_Vue_js
+
+# task 1
+https://codepen.io/AhmedHelalAhmed/pen/qgzzKR
+
+# task2
+https://codepen.io/AhmedHelalAhmed/pen/QYXeyW
+
+# task 3
+https://codepen.io/AhmedHelalAhmed/pen/KJjOBg
+
+# task 4
+https://codepen.io/AhmedHelalAhmed/pen/mvNbyd
+
+# task 5
+https://codepen.io/AhmedHelalAhmed/pen/qgeWPG
+
+# task 6
+https://codepen.io/AhmedHelalAhmed/pen/WPVeWz
+
